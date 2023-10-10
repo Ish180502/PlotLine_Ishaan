cd55f576ae22b8ee63f3231f3bc7bef8f4e8c0c2
Ish180502 <96649650+Ish180502@users.noreply.github.com>
2023-10-10 23:02:20 +0530

    Update setupProxy.js

--- SwiftServe/src/setupProxy.js
+++ SwiftServe/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'https://swift-serve-bt21.onrender.com/',
             changeOrigin: true,
         })
     );
