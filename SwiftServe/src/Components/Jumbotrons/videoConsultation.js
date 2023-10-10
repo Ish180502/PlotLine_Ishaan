f7218988e3bc0953ef0a8a11c43bb61c098b8f0d
Ish180502 <96649650+Ish180502@users.noreply.github.com>
2023-10-10 07:36:07 +0530

    Create videoConsultation.js

---
+++ SwiftServe/src/Components/Jumbotrons/videoConsultation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const VideoConsultationBanner = () => {
+
+    return (
+        <div className="container-fluid p-4  text-black justify-content-sm-between align-items-center justify-content-center text-center text-sm-start" style={{ background: "white" }}>
+            <h2 className="fs-2 ">Book Our Services</h2>
+            <h3 className="fs-5 pb-3 fw-light"><b>Contact isingh1_be20@thapar.edu for queries</b></h3>
+            <h4 className="fs-6 text-secondary">Available: 9:00am to 7:30pm</h4>
+            <h5 className="fs-6 text-secondary">Click "Next" in below mentioned option</h5>
+        </div>
+    )
+}
+export default VideoConsultationBanner
